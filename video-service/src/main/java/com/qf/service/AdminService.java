01d60d97a4d5d41ba0b7702d05e0b68ed19a0f49
grass9527 <1047509751@qq.com>
2020-10-19 16:50:19 +0800

    db

---
+++ video-service/src/main/java/com/qf/service/AdminService.java
@@ -0,0 +1,9 @@
+package com.qf.service;
+
+import com.qf.pojo.Admin;
+
+import java.util.List;
+
+public interface AdminService {
+    List<Admin> findAll();
+}
---
+++ video-service/src/main/java/com/qf/service/impl/AdminServiceImpl.java
@@ -0,0 +1,21 @@
+package com.qf.service.impl;
+
+import com.qf.dao.AdminMapper;
+import com.qf.pojo.Admin;
+import com.qf.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    private AdminMapper adminMapper;
+
+    @Override
+    public List<Admin> findAll() {
+        return adminMapper.selectByExample(null);
+    }
+}
