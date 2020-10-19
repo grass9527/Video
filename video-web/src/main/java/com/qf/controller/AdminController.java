fa9659b57d53bb2fb3af6e432873233e69fb54df
grass9527 <1047509751@qq.com>
2020-10-19 21:11:17 +0800

    login

--- video-web/src/main/java/com/qf/controller/AdminController.java
+++ video-web/src/main/java/com/qf/controller/AdminController.java
@@ -3,37 +3,40 @@ package com.qf.controller;
 import com.qf.pojo.Admin;
 import com.qf.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("admin")
 public class AdminController {
 
     @Autowired
     private AdminService adminService;
 
+    @ResponseBody
     @RequestMapping("login")
-    public ModelAndView login(@RequestParam (required = false) String username,
+    public String login(@RequestParam (required = false) String username,
                               @RequestParam (required = false) String password) {
         List<Admin> list = adminService.findAll();
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("data","false");
+
         for (Admin admin : list) {
             if (admin.getUsername().equals(username)&&admin.getPassword().equals(password)) {
-                mav.addObject("data","success");
+                return "success";
             }
         }
 
-        return mav;
+        return "false";
 
     }
 
     @RequestMapping("test")
+    @ResponseBody
     public ModelAndView test() {
 
         ModelAndView mav = new ModelAndView();
